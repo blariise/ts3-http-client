37fb06a4638cd0757d1b3a4345074fadf40f5b06
blariise <blariise1337@gmail.com>
2022-12-29 20:11:06 +0100

    add banclient query

--- commands.py
+++ commands.py
@@ -22,6 +22,45 @@ class Commands():
         }
         return self.query('apikeyadd', params)
 
+    def ban_add(
+        self,
+        ip: Optional[str] = None,
+        name: Optional[str] = None,
+        uid: Optional[int] = None,
+        mytsid: Optional[int] = None,
+        time: Optional[int] = None,
+        banreason: Optional[str] = None,
+        lastnickname: Optional[str] = None
+    ) -> Response:
+
+        params = {
+            'ip': ip,
+            'name': name,
+            'uid': uid,
+            'mytsid': mytsid,
+            'time': time,
+            'banreason': banreason,
+            'lastnickname': lastnickname
+        }
+        return self.query('banadd', params)
+
+    def ban_client(
+        self,
+        continueonerror: Optional[bool] = None,
+        clid: Optional[int] = None,
+        time: Optional[int] = None,
+        banreason: Optional[int] = None
+    ) -> Response:
+        params = {
+            'clid': clid,
+            'time': time,
+            'banreason': banreason
+        }
+        if continueonerror is True:
+            params['-continueonerror'] = ''
+
+        return self.query('banclient', params)
+
     def whoami(self) -> Response:
         return self.query('whoami')
         
